105256a99adb4b2a70adc2892c43c01312886c38
Daniele Quasimodo <ICTS21-23.241@studenti.local>
2022-03-10 11:53:44 +0100

    Aggiunto progetto Java

---
+++ src/main/Main.java
@@ -0,0 +1,45 @@
+package main;
+
+public class Main {
+	
+	
+	// Metodi
+	private static void alineri() {
+		
+	}
+	
+	private static void capulong() {
+		
+	}
+	
+	private static void nicolotti() {
+		
+	}
+	
+	private static void samime() {
+		
+	}
+	
+	private static void stella() {
+		
+	}
+	
+	private static void quasimodo() {
+		System.out.println("Ciao, sono Daniele Quasimodo");
+	}
+	
+	
+	
+	// MAIN
+	public static void main(String[] args) {
+		
+		alineri();
+		capulong();
+		nicolotti();
+		samime();
+		stella();
+		quasimodo();
+		
+	}
+
+}
